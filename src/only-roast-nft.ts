bd97e3311b6fd778a516afd70c42eabc17aa64a8
krishnan74 <divyakrishnan.r74@gmail.com>
2025-02-06 15:28:53 +0530

    Initial commit

---
+++ src/only-roast-nft.ts
@@ -0,0 +1,159 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  BatchMetadataUpdate as BatchMetadataUpdateEvent,
+  DropAdded as DropAddedEvent,
+  LitAdded as LitAddedEvent,
+  MetadataUpdate as MetadataUpdateEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  TokenMinted as TokenMintedEvent,
+  Transfer as TransferEvent
+} from "../generated/OnlyRoastNFT/OnlyRoastNFT"
+import {
+  Approval,
+  ApprovalForAll,
+  BatchMetadataUpdate,
+  DropAdded,
+  LitAdded,
+  MetadataUpdate,
+  OwnershipTransferred,
+  TokenMinted,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBatchMetadataUpdate(
+  event: BatchMetadataUpdateEvent
+): void {
+  let entity = new BatchMetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._fromTokenId = event.params._fromTokenId
+  entity._toTokenId = event.params._toTokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDropAdded(event: DropAddedEvent): void {
+  let entity = new DropAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.tokenId = event.params.tokenId
+  entity.newDropCount = event.params.newDropCount
+  entity.timestamp = event.params.timestamp
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleLitAdded(event: LitAddedEvent): void {
+  let entity = new LitAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.tokenId = event.params.tokenId
+  entity.newLitCount = event.params.newLitCount
+  entity.timestamp = event.params.timestamp
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
+  let entity = new MetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._tokenId = event.params._tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTokenMinted(event: TokenMintedEvent): void {
+  let entity = new TokenMinted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+  entity.uri = event.params.uri
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ tests/only-roast-nft-utils.ts
@@ -0,0 +1,241 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  BatchMetadataUpdate,
+  DropAdded,
+  LitAdded,
+  MetadataUpdate,
+  OwnershipTransferred,
+  TokenMinted,
+  Transfer
+} from "../generated/OnlyRoastNFT/OnlyRoastNFT"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createBatchMetadataUpdateEvent(
+  _fromTokenId: BigInt,
+  _toTokenId: BigInt
+): BatchMetadataUpdate {
+  let batchMetadataUpdateEvent = changetype<BatchMetadataUpdate>(newMockEvent())
+
+  batchMetadataUpdateEvent.parameters = new Array()
+
+  batchMetadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_fromTokenId",
+      ethereum.Value.fromUnsignedBigInt(_fromTokenId)
+    )
+  )
+  batchMetadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_toTokenId",
+      ethereum.Value.fromUnsignedBigInt(_toTokenId)
+    )
+  )
+
+  return batchMetadataUpdateEvent
+}
+
+export function createDropAddedEvent(
+  user: Address,
+  tokenId: BigInt,
+  newDropCount: BigInt,
+  timestamp: BigInt
+): DropAdded {
+  let dropAddedEvent = changetype<DropAdded>(newMockEvent())
+
+  dropAddedEvent.parameters = new Array()
+
+  dropAddedEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+  dropAddedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  dropAddedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newDropCount",
+      ethereum.Value.fromUnsignedBigInt(newDropCount)
+    )
+  )
+  dropAddedEvent.parameters.push(
+    new ethereum.EventParam(
+      "timestamp",
+      ethereum.Value.fromUnsignedBigInt(timestamp)
+    )
+  )
+
+  return dropAddedEvent
+}
+
+export function createLitAddedEvent(
+  user: Address,
+  tokenId: BigInt,
+  newLitCount: BigInt,
+  timestamp: BigInt
+): LitAdded {
+  let litAddedEvent = changetype<LitAdded>(newMockEvent())
+
+  litAddedEvent.parameters = new Array()
+
+  litAddedEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+  litAddedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  litAddedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newLitCount",
+      ethereum.Value.fromUnsignedBigInt(newLitCount)
+    )
+  )
+  litAddedEvent.parameters.push(
+    new ethereum.EventParam(
+      "timestamp",
+      ethereum.Value.fromUnsignedBigInt(timestamp)
+    )
+  )
+
+  return litAddedEvent
+}
+
+export function createMetadataUpdateEvent(_tokenId: BigInt): MetadataUpdate {
+  let metadataUpdateEvent = changetype<MetadataUpdate>(newMockEvent())
+
+  metadataUpdateEvent.parameters = new Array()
+
+  metadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_tokenId",
+      ethereum.Value.fromUnsignedBigInt(_tokenId)
+    )
+  )
+
+  return metadataUpdateEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent =
+    changetype<OwnershipTransferred>(newMockEvent())
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTokenMintedEvent(
+  to: Address,
+  tokenId: BigInt,
+  uri: string
+): TokenMinted {
+  let tokenMintedEvent = changetype<TokenMinted>(newMockEvent())
+
+  tokenMintedEvent.parameters = new Array()
+
+  tokenMintedEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  tokenMintedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  tokenMintedEvent.parameters.push(
+    new ethereum.EventParam("uri", ethereum.Value.fromString(uri))
+  )
+
+  return tokenMintedEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
